b2d381578db0c58e0edba7883a7e6f709e52a0d4
Brandon Stone <brandonrstone@gmail.com>
2022-07-25 19:41:43 -0400

    Added Mongoose, Schema for User

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+require('dotenv').config()
 
 app.use(express.json())
 app.use(express.urlencoded())
@@ -17,6 +18,13 @@ app.post('/name', (req, res) => {
   }
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to database!')
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`)
+    })
+  })
+  .catch(err => console.log(err))
