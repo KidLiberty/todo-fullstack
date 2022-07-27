f02cc0fa72a6accbc50608a0d870327b6774865e
Brandon Stone <brandonrstone@gmail.com>
2022-07-27 10:49:03 -0400

    Added auth middleware for JWT requests

---
+++ middleware/permissions.js
@@ -0,0 +1,39 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+
+/* 
+    next() continues the request instead of cutting it early, 
+    passing it to the next handler (param) in the function
+*/
+const requiresAuth = async (req, res, next) => {
+  const token = req.cookies['access-token']
+  let isAuthed = false
+
+  if (token) {
+    try {
+      const { userId } = jwt.verify(token, process.env.JWT_SECRET)
+      try {
+        const user = await User.findById(userId)
+
+        if (user) {
+          const userToReturn = { ...user._doc }
+          delete userToReturn.password
+          req.user = userToReturn
+          isAuthed = true
+        }
+      } catch {
+        isAuthed = false
+      }
+    } catch {
+      isAuthed = false
+    }
+  }
+
+  if (isAuthed) {
+    return next()
+  } else {
+    return res.status(401).send('Unauthorized')
+  }
+}
+
+module.exports = requiresAuth
